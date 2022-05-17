037337ec0212c16ee4d5584381d7da510031ab37
pierre-alexandre <pa.detollenaere@gmail.com>
2022-05-17 09:20:28 +0200

    détail des autres fonctions à connaître

---
+++ script8.js
@@ -0,0 +1,28 @@
+/**
+ * Les variables : typeof(variable)
+ * Booleen
+ * Tableau
+ * Objet
+ * 
+ * Notion de portée de variable : 
+ * const
+ * let
+ * var
+ * 
+ * Fonctions :
+ * La notion de parametre
+ * Une fonction peut renvoyer quelque chose ou pas
+ * Algorithmie : somme, tri
+ * Callback
+ * 
+ * DOM :
+ * document.getElementById()
+ * document.getElementsByClassName()
+ * document.GetElementsByTag()
+ * 
+ * Conclusion :
+ * Jquery
+ * Nodejs
+ * 
+ * 
+ */
